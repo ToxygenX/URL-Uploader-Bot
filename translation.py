07b85a73ffdaeb9436cf084422bc5278946c388e
Fayas Kallekkad <76828314+FayasKallekkad@users.noreply.github.com>
2021-01-03 10:16:01 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -6,10 +6,7 @@ class Translation(object):
 
 /help for more details..
 """
- 
-
-
-RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+    RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
