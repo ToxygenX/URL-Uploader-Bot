ea8e8e8354d8363aa084dced838fa44762ba35eb
Fayas Kallekkad <76828314+FayasKallekkad@users.noreply.github.com>
2021-01-03 10:19:12 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -68,3 +68,4 @@ You can use /rename command after receiving file to rename it with custom thumbn
 Free users only 1 request per 30 minutes.
 /upgrade or Try 1800 seconds later."""
     SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    BANNED_USER_TEXT = "You are Banned!"
