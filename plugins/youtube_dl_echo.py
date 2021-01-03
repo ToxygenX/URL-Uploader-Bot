f81bf0ecfe4bd3db0acbd180f954165af1a7ce8d
Fayas Kallekkad <76828314+FayasKallekkad@users.noreply.github.com>
2021-01-03 10:17:40 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -39,25 +39,6 @@ async def echo(bot, update):
         await update.reply_text("You are B A N N E D 🤣🤣🤣🤣")
         return
     TRChatBase(update.from_user.id, update.text, "/echo")
-    update_channel = Config.UPDATE_CHANNEL
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked":
-               await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
-               return
-        except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-            await update.reply_text(
-                text="**Join My Updates Channel to use ME 😎 🤭**",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
-              ])
-            )
-            return
-        except Exception:
-            await update.reply_text("Something Wrong. Contact my Support Group")
-            return
     logger.info(update.from_user)
     url = update.text
     youtube_dl_username = None
